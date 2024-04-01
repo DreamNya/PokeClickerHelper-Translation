17b26f70f02d956172c86cf289dbc373535b1027
DreamNya <steven026@vip.qq.com>
2024-04-01 16:11:29 +0800

    更新 脚本 补充QuestLine

--- 宝可梦点击（Poke Clicker）内核汉化脚本.user.js
+++ 宝可梦点击（Poke Clicker）内核汉化脚本.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         宝可梦点击（Poke Clicker）内核汉化脚本
 // @namespace    PokeClickerHelper
-// @version      0.10.19-a
+// @version      0.10.19-b
 // @description  采用内核汉化形式，目前汉化范围：所有任务线、城镇名
 // @author       DreamNya, ICEYe, iktsuarpok, 我是谁？, 顶不住了, 银☆星
 // @match        http://localhost:3000/
@@ -17,24 +17,27 @@
 // @license      MIT
 // @connect      cdn.jsdelivr.net
 // ==/UserScript==
-/* global TownList, QuestLine:true , Notifier */
+/* global TownList, QuestLine:true, Notifier, MultipleQuestsQuest, App */
 
 //储存汉化文本
 const Translation = {};
-const TranslationHelper = { Translation };
+const TranslationHelper = { Translation, exporting: false };
 (window.PokeClickerHelper || window.PokeClickerHelperPlus || window).TranslationHelper = TranslationHelper;
 
 // 引用外部资源
 // CDN: https://cdn.jsdelivr.net
 // GIT: https://github.com/DreamNya/PokeClickerHelper-Translation
 const resources = ["QuestLine", "Town"];
+const now = Date.now();
 
 for (const resource of resources) {
     Translation[resource] = await FetchResource(resource).catch(() => {
         const cache = localStorage.getItem(`PokeClickerHelper-Translation-${resource}`);
         if (cache) {
+            console.log("PokeClickerHelper-Translation", "fallback获取json", resource);
             return JSON.parse(cache);
         } else {
+            console.log("PokeClickerHelper-Translation", "all failed获取json", resource);
             Notifier.notify({
                 title: "宝可梦点击（Poke Clicker）内核汉化脚本",
                 message: `请求汉化json失败，请检查网络链接或更新脚本\n无法完成汉化：${resource}`,
@@ -45,19 +48,26 @@ for (const resource of resources) {
     });
 }
 
-function FetchResource(resource) {
-    // eslint-disable-next-line no-async-promise-executor
-    return new Promise(async (resolve, reject) => {
-        const url = `https://cdn.jsdelivr.net/gh/DreamNya/PokeClickerHelper-Translation/json/${resource}.json`;
-        const response = await fetch(url);
-        if (response.status == 200) {
-            const json = await response.json();
-            localStorage.setItem(`PokeClickerHelper-Translation-${resource}`, JSON.stringify(json));
-            return resolve(json);
-        } else {
-            return reject();
+async function FetchResource(resource) {
+    const past = +localStorage.getItem(`PokeClickerHelper-Translation-${resource}-lastModified`) ?? 0;
+    if (now - past <= 86400 * 3 * 1000) {
+        const cache = localStorage.getItem(`PokeClickerHelper-Translation-${resource}`);
+        if (cache) {
+            console.log("PokeClickerHelper-Translation", "从存储获取json", resource);
+            return JSON.parse(cache);
         }
-    });
+    }
+    const url = `https://cdn.jsdelivr.net/gh/DreamNya/PokeClickerHelper-Translation/json/${resource}.json`;
+    const response = await fetch(url);
+    if (response.status == 200) {
+        const json = await response.json();
+        console.log("PokeClickerHelper-Translation", "从CDN获取json", resource);
+        localStorage.setItem(`PokeClickerHelper-Translation-${resource}`, JSON.stringify(json));
+        localStorage.setItem(`PokeClickerHelper-Translation-${resource}-lastModified`, now);
+        return json;
+    } else {
+        throw new Error();
+    }
 }
 
 // 汉化城镇
@@ -83,7 +93,18 @@ QuestLine.prototype.addQuest = new Proxy(QuestLine.prototype.realAddQuest, {
             const displayDescription = translation.descriptions[description];
             if (displayDescription) {
                 Object.defineProperty(quest, "description", {
-                    get: () => displayDescription,
+                    get: () => (TranslationHelper.exporting ? description : displayDescription),
+                });
+            }
+            if (quest instanceof MultipleQuestsQuest) {
+                quest.quests.forEach((q) => {
+                    const description = quest.description;
+                    const displayDescription = translation.descriptions[description];
+                    if (displayDescription) {
+                        Object.defineProperty(q, "description", {
+                            get: () => (TranslationHelper.exporting ? description : displayDescription),
+                        });
+                    }
                 });
             }
         }
@@ -101,12 +122,12 @@ QuestLine = new Proxy(window.realQuestLine, {
         const displayName = translation?.name;
         const displayDescription = translation?.description[description];
         Object.defineProperty(questline, "displayName", {
-            get: () => displayName ?? name,
+            get: () => (TranslationHelper.exporting ? name : displayName ?? name),
         });
 
         if (displayDescription) {
             Object.defineProperty(questline, "description", {
-                get: () => displayDescription,
+                get: () => (TranslationHelper.exporting ? description : displayDescription),
             });
         }
 
@@ -120,6 +141,9 @@ document.querySelector(
 ).dataset.bind = "text: $data.displayName";
 document.querySelector("#bulletinBoardModal div.modal-body h5[data-bind='text: $data.name']").dataset.bind =
     "text: $data.displayName";
+document
+    .querySelectorAll('#questsModalQuestLinesPane knockout.font-weight-bold.d-block[data-bind="text: $data.name"]')
+    .forEach((i) => (i.dataset.bind = "text: $data.displayName"));
 
 /*
 Translations = Object.assign({}, ...Object.values(Translation));
@@ -132,3 +156,28 @@ q.reduce((obj, questline) => {
     return obj;
 }, {});
 */
+
+TranslationHelper.ExportTranslation = {};
+TranslationHelper.ExportTranslation.QuestLine = function () {
+    TranslationHelper.exporting = true;
+    const json = App.game.quests.questLines().reduce((obj, questline) => {
+        const { name, description } = questline;
+        const translation = Translation.QuestLine[name];
+        const subObj = {};
+        subObj.name = translation.name ?? name;
+        subObj.description = { [description]: translation.description[description] ?? "" };
+        subObj.descriptions = questline.quests().reduce((d, q) => {
+            d[q.description] = translation.descriptions[q.description] ?? "";
+            if (q instanceof MultipleQuestsQuest) {
+                q.quests.forEach((qq) => {
+                    d[qq.description] = translation.descriptions[qq.description] ?? "";
+                });
+            }
+            return d;
+        }, {});
+        obj[name] = subObj;
+        return obj;
+    }, {});
+    TranslationHelper.exporting = false;
+    return json;
+};
