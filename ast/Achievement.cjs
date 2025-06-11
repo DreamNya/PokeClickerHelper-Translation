2b3944d1fd7d5e76cee892cb5b1bba87e260c6cc
DreamNya <steven026@vip.qq.com>
2025-06-11 18:11:11 +0800

    优化 AST 正则匹配方式
    更精准的正则优先匹配

--- ast/Achievement.cjs
+++ ast/Achievement.cjs
@@ -1,7 +1,7 @@
-const { Project, SyntaxKind } = require('ts-morph');
-const fs = require('fs');
+const { Project, SyntaxKind } = require("ts-morph");
+const fs = require("fs");
 
-const filePath = 'json/Achievement.json';
+const filePath = "json/Achievement.json";
 
 const Translation = fs.existsSync(filePath)
     ? JSON.parse(fs.readFileSync(filePath))
@@ -13,13 +13,13 @@ const Translation = fs.existsSync(filePath)
       };
 
 const project = new Project();
-const sourceFile = project.addSourceFileAtPath('./pokeclicker/src/scripts/achievements/AchievementHandler.ts');
+const sourceFile = project.addSourceFileAtPath("./pokeclicker/src/scripts/achievements/AchievementHandler.ts");
 
 // 查找函数调用
 const calls = sourceFile.getDescendantsOfKind(SyntaxKind.CallExpression).filter((call) => {
     const expression = call.getExpression();
     const expressionName = expression.getText();
-    return expressionName == 'AchievementHandler.addAchievement';
+    return expressionName == "AchievementHandler.addAchievement";
 });
 
 function formatString(template) {
@@ -32,7 +32,7 @@ function formatString(template) {
             const head = template.getHead().getLiteralText(); // .replace(/\./g,'\\.');
             const text = template
                 .getTemplateSpans()
-                .reduce((text, span) => text + '(.*?)' + span.getLiteral().getLiteralText(), head);
+                .reduce((text, span) => text + "(.*?)" + span.getLiteral().getLiteralText(), head);
 
             return `^${text}$`;
         default:
@@ -45,22 +45,26 @@ const results = calls.reduce(
         const [nameTemplate, descriptionTemplate] = call.getArguments();
 
         const name = formatString(nameTemplate);
-        if (name.startsWith('^')) {
-            obj.nameReg[name] = Translation.nameReg[name] || '';
+        if (name.startsWith("^")) {
+            obj.nameReg.push([name, Translation.nameReg[name] || ""]);
         } else {
-            obj.name[name] = Translation.name[name] || '';
+            obj.name[name] = Translation.name[name] || "";
         }
 
         const description = formatString(descriptionTemplate);
-        if (description.startsWith('^')) {
-            obj.descriptionReg[description] = Translation.descriptionReg[description] || '';
+        if (description.startsWith("^")) {
+            obj.descriptionReg.push([description, Translation.descriptionReg[description] || ""]);
         } else {
-            obj.description[description] = Translation.description[description] || '';
+            obj.description[description] = Translation.description[description] || "";
         }
 
         return obj;
     },
-    { name: {}, nameReg: {}, description: {}, descriptionReg: {} },
+    { name: {}, nameReg: [], description: {}, descriptionReg: [] }
 );
+
+// 更精准的正则优先匹配 简单量化为正则长度
+results.nameReg = Object.fromEntries(results.nameReg.sort(([a], [b]) => b.length - a.length));
+results.descriptionReg = Object.fromEntries(results.descriptionReg.sort(([a], [b]) => b.length - a.length));
 
 fs.writeFileSync(filePath, JSON.stringify(results, null, 4));
